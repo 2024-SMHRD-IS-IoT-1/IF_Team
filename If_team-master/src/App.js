ce83ee6e8b7be75a9fbb19894d5d2013e08ef4e9
DW <danielwerner102366@gmail.com>
2024-09-03 17:33:33 +0900

    dw

---
+++ If_team-master/src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './Components/LoginSignup/HomePage';
+import LoginPage from './Components/LoginSignup/LoginPage';
+import ForgotID from './Components/LoginSignup/ForgotID';
+import ForgotPW from './Components/LoginSignup/ForgotPW';
+
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="/forgot-id" element={<ForgotID />} />
+      <Route path="/forgot-password" element={<ForgotPW />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ If_team-master/src/Components/LoginSignup/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css';
+const nav= useNavigate();
+const Main = () => {
+  return (
+    <div className="container">
+      <header>
+        <h1>스마트 식물 무드등</h1>
+      </header>
+      <nav className="navbar">
+        <ul>
+          <li><a href="#home" >Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services" >Services</a></li>
+          <li><a href="#contact" >Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Main
