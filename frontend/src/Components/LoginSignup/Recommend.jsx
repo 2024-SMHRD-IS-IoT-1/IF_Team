da6e328fe4a40f24545cfc3670a185982547ac4d
DW <danielwerner102366@gmail.com>
2024-09-04 15:45:01 +0900

    dw

--- frontend/src/Components/LoginSignup/Recommend.jsx
+++ frontend/src/Components/LoginSignup/Recommend.jsx
@@ -6,8 +6,6 @@ const products = [
   { id: 1, name: 'Product 1', price: '$100', imageUrl: 'https://via.placeholder.com/150' },
   { id: 2, name: 'Product 2', price: '$150', imageUrl: 'https://via.placeholder.com/150' },
   { id: 3, name: 'Product 3', price: '$200', imageUrl: 'https://via.placeholder.com/150' },
-  { id: 4, name: 'Product 4', price: '$200', imageUrl: 'https://via.placeholder.com/150' },
-  { id: 5, name: 'Product 5', price: '$200', imageUrl: 'https://via.placeholder.com/150' },
   // 필요한 만큼 더 추가
 ];
 
