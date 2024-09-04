95dbf8dadb34344b840791ad43e6a816deef76a6
daa <k01997@naver.com>
2024-09-04 18:55:20 +0900

    다영

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,20 @@ import ForgotID from './Components/LoginSignup/ForgotID';
 import ForgotPW from './Components/LoginSignup/ForgotPW';
 import Signup from './Components/LoginSignup/Signup';
 import Recommend from './Components/LoginSignup/Recommend';
-
+import Main from './Components/LoginSignup/Main';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/homepage" element={<HomePage />} />
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/forgot-id" element={<ForgotID />} />
       <Route path="/forgot-password" element={<ForgotPW />} />
       <Route path="/Signup" element={<Signup />} />
       <Route path="/Recommend" element={<Recommend />} />
+ 
     </Routes>
   );
 }
