576ef2515fafbefcd07cb501733b631a8ba83a78
조성일 <xblue9259@gmail.com>
2024-09-04 11:23:02 +0900

    불러옴

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -1,19 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const main = () => {
+const Main = () => {
   return (
-<<<<<<< HEAD
-    <div>main</div>
-  )
-}
-
-export default main
-=======
     <div>
-      dada
+      <div>main</div>
     </div>
-  )
-}
+  );
+};
 
-export default main
->>>>>>> bf67c2d0808f55d33defe8b61414cd71fc83e1f2
+export default Main;
