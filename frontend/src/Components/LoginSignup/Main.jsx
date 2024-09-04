8e927d65e68d8eb90d3044b92b8b5c2d504c1eaa
daa <k01997@naver.com>
2024-09-04 23:06:31 +0900

    main수정

--- frontend/src/Components/LoginSignup/Main.jsx
+++ frontend/src/Components/LoginSignup/Main.jsx
@@ -1,28 +1,30 @@
+// 웹 첫 시작 페이지
+
+
 import React from 'react';
 import { useNavigate, navigate } from 'react-router-dom'; // useNavigate 훅 임포트
 import '../../css/main.css'
-
-
+//아이콘
 import { HiOutlineHome } from "react-icons/hi";
+
 
 const Main = () => {
   const navigate = useNavigate();
 
   const handleHomeClick = () => {
   navigate('/Hompage'); 
-  
 };
 
 
   return (
-    <div className="container">
-      <header className='header'>
-          <h1 className='title'>Make your</h1> 
-          <h1>Mood Light</h1>
+    <div className="main-container">
+      <header className='main-header'>
+          <h1 className='main-title'>Make your</h1> 
+          <h1 className="main-title">Mood Light</h1>
           <hr></hr>
       </header>
       
-      <button className='btn-home' type='button' onClick={handleHomeClick} >
+      <button className='main-btnhome' type='button' onClick={handleHomeClick} >
         <HiOutlineHome size={60}/>
       </button>
     </div>
