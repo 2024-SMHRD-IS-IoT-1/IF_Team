65ae015f59aa10893f0ec8c50c3112a2c373f8ba
kimsj <kimsj020406@gmail.com>
2024-09-05 17:38:50 +0900

    로그인기능

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ const app = express();
 const bp = require("body-parser");
 // routes 경로에 있는 파일 호출
 const userRouter = require("./routes/userRouter");
+const session = require('express-session');
+const fileStore = require('session-file-store')(session);
 
 // cors 설정 ( 도메인 주소가 달라도 일치 시킬 수 있음)
 const cors = require('cors')
-const router = express.Router();
-  router.get("/checkAuth", (req, res) => {
-    if (req.session.user_id) {
-        res.json({ loggedIn: true, user_id: req.session.user_id });
-    } else {
-        res.json({ loggedIn: false });
-    }
-  });
+app.use(session({
+    httpOnly: true,
+    resave: false,
+    secret: 'secret',
+    store: new fileStore(),
+    saveUninitialized: false,
+    expires: new Date(Date.now() + (60 * 60 * 24 * 7 * 1000)),
+}));
 app.use(cors());
 app.use(bp.urlencoded({extended : true})); 
 app.use(express.json());
@@ -23,7 +25,16 @@ app.use("/user",userRouter);
 // 메인페이지 경로 설정 
 /*const Router = require('./routes/userRouter')
 app.use('/', Router);*/
-
+// // 세션 등록하기! 
+// app.use(session({
+//     httpOnly : true, //http로 들어온 요청만 처리하겠다. 
+//     resave : false, //세션을 항상 재저장 할 지 확인
+//     secret : "secret", //암호화할때 사용된 키 값
+//     store : new fileStore(), //세션을 저장할 공간을 할당 
+//     saveUninitialized : false // 세션이 비어있더라도 저장 할거냐!🚧(중요)
+// }))
+// app.use("/",userRouter);
+// app.use("/cookie",cookie);
 // 리액트 프로젝트 경로 설정 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
@@ -33,4 +44,6 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), ()=>{
     console.log(`Server is running on ${app.get('port')}`);
+    
+
 })
