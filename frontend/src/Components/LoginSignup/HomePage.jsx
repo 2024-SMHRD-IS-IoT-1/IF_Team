0b4d4dd8ed63e99eade25748f5c55526d1ef5185
DW <danielwerner102366@gmail.com>
2024-09-05 12:00:35 +0900

    dw

--- frontend/src/Components/LoginSignup/HomePage.jsx
+++ frontend/src/Components/LoginSignup/HomePage.jsx
@@ -23,6 +23,10 @@ const HomePage = () => {
     navigate('/Signup') // Signup page로 이동 
   };
 
+  const handleHomeClick = () => {
+    navigate('/'); // 홈페이지로 이동
+  };
+
   return (
     <div className="homepage-container">
       <header className="homepage-header">
@@ -30,47 +34,30 @@ const HomePage = () => {
           <h2 id='title2'>MOOD LIGHT</h2>
       </header>
       
-    <div className='homepage-menu'>
-      <div className='menu-icon'>
-        <HiOutlineHome size={40}/>
-        <div className='menu-text'>Home</div>
-      </div>
-      <div className='menu-icon'>
-        <MdOutlineRateReview size={40}/>
-        <div className='menu-text'>Info</div>
-      </div>
-      <div className="menu-icon">
-          <FaCog size={40} />
-          <div className="menu-text">Settings</div>
+      <div className='homepage-menu'>
+        <div className='menu-icon' onClick={handleHomeClick}> {/* 클릭 이벤트 추가 */}
+          <HiOutlineHome size={40}/>
+          <div className='menu-text'>Home</div>
         </div>
-      <div className='menu-icon'>
-        <FaCommentDots size={40}/>
-      <div className='menu-text'>comments</div>
+        <div className='menu-icon'>
+          <MdOutlineRateReview size={40}/>
+          <div className='menu-text'>Info</div>
+        </div>
+        <div className="menu-icon">
+            <FaCog size={40} />
+            <div className="menu-text">Settings</div>
+          </div>
+        <div className='menu-icon'>
+          <FaCommentDots size={40}/>
+        <div className='menu-text'>comments</div>
+        </div>
       </div>
-    </div>
 
-    {/* 로그인 버튼 */}
-    <div className='homepage-buttion-container'>
-      <button className='homepage-btn-login' type='button' onClick={handleLoginClick}>LOGIN</button>
-      <div onClick={handleSignup} className='homepage-Loginlink'>더 많은 기능을 사용하고 싶다면 ? sign up</div>
-    
-    </div>
-  
-
-
-      {/* <nav className="navbar">
-        <ul className="menu">
-          <Link to="/" className='submenu'>
-           <HiOutlineHome size={30}/></Link>
-          <Link to="/Recommend" className='submenu'>추천제품</Link>
-          <Link to="/Product" className='submenu'>제품 설명</Link>
-          <Link to="#contact" className='submenu'>Contact</Link>
-        </ul>
-      </nav>
-      <div className="button-container">
-        <button className="btn login-btn" type="button" onClick={handleLoginClick}>로그인</button>
-        <button className="btn signup-btn" type="button" onClick={handleSiginupClick}>회원가입</button>
-      </div> */}
+      {/* 로그인 버튼 */}
+      <div className='homepage-buttion-container'>
+        <button className='homepage-btn-login' type='button' onClick={handleLoginClick}>LOGIN</button>
+        <div onClick={handleSignup} className='homepage-Loginlink'>더 많은 기능을 사용하고 싶다면 ? sign up</div>
+      </div>
     </div>
   );
 }
