ebed2e02f54a2520cf36ab5824e242c5f5032154
daa <k01997@naver.com>
2024-09-08 11:10:47 +0900

    리뷰작성완료

--- frontend/src/Components/Feedback&Review/ReviewWrite.jsx
+++ frontend/src/Components/Feedback&Review/ReviewWrite.jsx
@@ -55,6 +55,19 @@ function ReviewWrite() {
       });
   }, []);
   */}
+   // 선호도 조사(preference)에 대한 상태
+   const [preference1, setPreference1] = useState('');
+   const [preference2, setPreference2] = useState('');
+   const [preference3, setPreference3] = useState('');
+   //리뷰선호도 조사 질문선택하기
+   const handlePreferenceCilck = (setPreference, value) => {
+       setPreference(value); //선택된 값으로 상태를 업데이트
+   };
+  // 리뷰선호도 조사 질문선택하기
+  const handlePreferenceClick = (setPreference, value) => {
+    setPreference(value); // 선택된 값으로 상태를 업데이트
+  };
+
   // 리뷰 저장 처리 함수
   const handleSaveReview = () => {
     // if (!isLoggedIn) {
@@ -110,7 +123,110 @@ function ReviewWrite() {
             <hr />
             {/* 리뷰 선택 작성 */}
             {/* 선택 질문 생략 (필요시 추가) */}
-          </div>
+              {/* 리뷰 선택 작성  */}
+    <div className='chioce-contents'>
+      <h2 className='reviewwrite-preference'>스마트 플랜테리어 무드등선호도 조사</h2>
+      <h1 className='reviewwrite-preference'>선호도 조사</h1>
+         
+      {/* 질문 1 */}
+      <div className='question'>
+        <p>제품 사용 후 변화가 있었나요?</p>
+        <label>
+        <input
+          type='radio'
+          value='만족'
+          checked={preference1 === '만족'}
+          onChange={() => handlePreferenceCilck(setPreference1, '만족')}
+          />
+          만족
+        </label>
+        <label>
+        <input
+          type='radio'
+          value='보통'
+          checked={preference1 === '보통'}
+          onChange={() => handlePreferenceCilck(setPreference1, '보통')}
+        />
+        보통
+        </label>
+        <label>
+        <input
+          type='radio'
+          value='불만족'
+          checked={preference1 === '불만족'}
+          onChange={() => handlePreferenceCilck(setPreference1, '불만족')}
+        />
+        불만족
+        </label>
+        </div>
+
+      {/* 질문 2 */}
+        <div className='question'>
+        <p>제품 디자인에 만족하나요?</p>
+        <label>
+          <input
+            type='radio'
+            value='만족'
+            checked={preference2 === '만족'}
+            onChange={() => handlePreferenceCilck(setPreference2, '만족')}
+          />
+          만족
+        </label>
+        <label>
+          <input
+            type='radio'
+            value='보통'
+            checked={preference2 === '보통'}
+            onChange={() => handlePreferenceCilck(setPreference2, '보통')}
+          />
+          보통
+        </label>
+        <label>
+          <input
+            type='radio'
+            value='불만족'
+            checked={preference2 === '불만족'}
+            onChange={() => handlePreferenceCilck(setPreference2, '불만족')}
+          />
+          불만족
+        </label>
+        </div>
+
+      {/* 질문 3 */}
+        <div className='question'>
+            <p>제품 기능에 만족하나요?</p>
+            <label>
+            <input
+            type='radio'
+            value='만족'
+            checked={preference3 === '만족'}
+            onChange={() => handlePreferenceCilck(setPreference3, '만족')}
+            />
+            만족
+            </label>
+            <label>
+            <input
+            type='radio'
+            value='보통'
+            checked={preference3 === '보통'}
+            onChange={() => handlePreferenceCilck(setPreference3, '보통')}
+            />
+            보통
+            </label>
+            <label>
+            <input
+            type='radio'
+            value='불만족'
+            checked={preference3 === '불만족'}
+            onChange={() => handlePreferenceCilck(setPreference3, '불만족')}
+            />
+            불만족
+            </label>
+        </div>
+
+    </div>
+  </div>
+          
           <div className='button-container'>
             <button id='save-btn' onClick={handleSaveReview}>저장</button>
             <button id='cancel-btn' onClick={() => navigate('/reviewlist')}>취소</button>
