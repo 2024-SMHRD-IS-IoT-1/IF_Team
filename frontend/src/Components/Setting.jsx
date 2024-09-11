c374879d933383d7efecf4faba55c91271084b22
daa <k01997@naver.com>
2024-09-11 11:48:12 +0900

    다영

--- frontend/src/Components/Setting.jsx
+++ frontend/src/Components/Setting.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 // import TempHum from '../../../ardoino/templates/TempHum.html'
 // import TempHum_Ardo from '../../../ardoino/ardo/t_h_sensor.ino'
+import axios from 'axios';
 import '../css/setting.css'
 
 const Setting = () => {
@@ -12,6 +13,7 @@ const Setting = () => {
   // JWT 토큰 가져오기
   const token = localStorage.getItem('token');
   // JWT가 유효한지 체크하여 로그인 상태 설정 및 사용자 ID 추출
+  
   useEffect(() => {
     if (token) {
       try {
@@ -30,27 +32,82 @@ const Setting = () => {
     }
   }, [token]);
 
-  return (
+
+  //  현재 위치에 따른 날씨 데이터
+  const [temp, setTemp] = useState('0')
+  const [icon, setIcon] = useState('')
+  const [cloud, setCloud] = useState('')
+
+ 
+  const getData = (latitude, longitude) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=603aa562f93c1b6e5fb4e7596aa820d5`;
+    
+    axios
+    .get(url)
+    .then((res) => {
+         console.log(res);
+         // 구름의 양 -> 값 확인
+         console.log(res.data.clouds.all);
+         
+         // 켈빈 온도 - 273 = 섭씨 온도
+         // 온도-> temp
+         setTemp(parseInt(res.data.main.temp-273));
+         
+         // 아이콘 셋팅
+         // 주소 값으로 셋팅
+         setIcon(`https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`)
+         let clouds = res.data.clouds.all
+         if (clouds > 90){
+             setCloud('매우 흐림')
+         } else if (clouds > 60){
+             setCloud('흐림')
+         } else if (clouds > 30){
+             setCloud('약간 흐림')
+         } else {
+             setCloud('맑음')
+         }
+    })
+ }
+
+ useEffect(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        getData(latitude, longitude); // 위치를 기반으로 데이터 가져오기
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+}, []);
+    return (
    
     <div className='setting-container'>
         <div className='setting-header'>
-            <h1 id='setting-title2'>MOOD LIGHT</h1>
-            <h2 id='setting-title1'> Setting </h2>
+            <h1 id='setting-title1'>MOOD LIGHT</h1>
+            <h2 id='setting-title2'> Setting </h2>
         </div>
         <div className='setting-today-weather'>
           <div className='weater-box'>
             <h4>오늘의 날씨</h4 >
-            {/* <div className='weather-data'>
+            <div className='weather-data'>
             <img src={icon}></img>
-            <h1>{temp}</h1>
+            <h1>{temp}ºC</h1>
             <h4>{cloud}</h4>
-            </div> */}
+            </div>
           </div>
         </div>
         <div className='setting-menubox'>
-            <div className='setting-temp' >Temperpature</div>
-            <div className='setting-hum'>Humidity</div>
-            <div className='setting-led'>LED 조절</div>
+            <div className='setting-temp' >Temperpature : {temp}ºC</div>
+            <div className='setting-hum'>Humidity : </div>
+            <div className='setting-led'>
+              <h4>LED 조절</h4>
+
+              </div>
         </div>
         <div>
           <h2>나의 수면환경은?</h2>
@@ -60,5 +117,6 @@ const Setting = () => {
   
   )
 }
+
 export default Setting;
 
